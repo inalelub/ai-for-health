b3360750a6f3f1a98456803332155d752f1e45a1
Hetile Chuma <u20681047@up.ac.za>
2025-07-29 19:55:54 +0200

    ViewModels

---
+++ ai-for-health/ViewModels/UserProfileVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroHack_API.ViewModels
+{
+    public class UserProfileVM
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        public bool IsDarkModeEnabled { get; set; }
+
+        [RegularExpression("Email|SMS|None", ErrorMessage = "Communication preference must be 'Email', 'SMS', or 'None'.")]
+        public string CommunicationPreference { get; set; }
+
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; } 
+
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "New password must be at least 8 characters long.")]
+        public string NewPassword { get; set; }
+    }
+}
---
+++ ai-for-health/ViewModels/UserVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroHack_API.ViewModels
+{
+    public class UserVM
+    {
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Emailaddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+    }
+}
