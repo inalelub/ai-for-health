757c67d69b37664c9666ccc0edaec42d96cf3595
Hetile Chuma <u20681047@up.ac.za>
2025-07-29 19:54:17 +0200

    Models

---
+++ ai-for-health/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MicroHack_API.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string firstName { get; set; }
+        public bool IsDarkModeEnabled { get; set; } = false;
+        public string CommunicationPreference { get; set; } = "Email";
+    }
+}
---
+++ ai-for-health/Models/HealthCheck.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroHack_API.Models
+{
+    public class HealthCheck
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
