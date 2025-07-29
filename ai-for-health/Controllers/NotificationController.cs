adc1b3a5aed5c2fbda39f518005ee8a4dc45b897
Bulelani Botman <69175349+inalelub@users.noreply.github.com>
2025-07-29 17:14:51 +0200

    feat: add controllers for health checks and diagnostic tests
    - Introduced `DatabaseController` for health check API.
    - Added `DiagnosticTestController` for CRUD operations on diagnostic tests.
    - Implemented `NotificationController` for managing notifications.
    - All controllers utilize dependency injection with `ApplicationDbContext`.
    - Included necessary using directives for proper functionality.

---
+++ ai-for-health/Controllers/NotificationController.cs
@@ -0,0 +1,37 @@
+﻿using ai_for_health.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ai_for_health.Controllers
+{
+    [ApiController]
+    [Route("api/notifications")]
+    public class NotificationController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NotificationController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+
+        [HttpGet("/")]
+        public async Task<IResult> GetAll()
+        {
+            return TypedResults.Ok(await _db.Notifications.ToListAsync());
+        }
+
+        [HttpGet("/{id}")]
+        public async Task<IResult> GetById(int id)
+        {
+            var obj = await _db.Notifications.FindAsync(id);
+
+            if (obj is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            return TypedResults.Ok(obj);
+        }
+    }
+}
